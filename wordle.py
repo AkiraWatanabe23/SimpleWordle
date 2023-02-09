135feda237568331b681d830b5936007e7f01ccd
Akira Watanabe <a0323w@gmail.com>
2023-02-09 12:03:52 +0900

    単語追加

--- wordle.py
+++ wordle.py
@@ -7,7 +7,8 @@ words = ["above", "adult", "adapt", "brave", "build",
          "point", "snake", "unity", "slime", "false",
          "earth", "mouse", "horse", "smart", "clean",
          "shift", "space", "enter", "sweat", "berry",
-         "print", "movie", "break", "lemon", "smile"]
+         "print", "movie", "break", "lemon", "smile",
+         "paint", "order", "click", "shoes", "shirt"]
 
 def check(ans) -> bool:
     '''入力が答えと合っているか判定'''
