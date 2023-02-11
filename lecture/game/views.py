c7c2676c1168d7496269b4cc0dc7ef275ce27fe4
Akira Watanabe <a0323w@gmail.com>
2023-02-11 16:29:44 +0900

    入力を反映させるテスト中

--- lecture/game/views.py
+++ lecture/game/views.py
@@ -1,7 +1,12 @@
 '''実行する内容'''
+from django import forms
 from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    '追加内容の追加、反映'
+    task = forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
@@ -12,5 +17,7 @@ def index(request):
 
 def add(request):
     '''Listに要素を追加する'''
-    return render(request, "game/add.html")
+    return render(request, "game/add.html", {
+        "form": NewTaskForm()
+    })
     
