6f0d06bd6954fcce7a5aa1b8b8c33e20894d893a
Akira Watanabe <a0323w@gmail.com>
2023-02-11 16:02:24 +0900

    テスト中

--- lecture/game/views.py
+++ lecture/game/views.py
@@ -1,3 +1,16 @@
+'''実行する内容'''
 from django.shortcuts import render
 
+tasks = ["foo", "bar", "baz"]
+
 # Create your views here.
+def index(request):
+    '''Listの表示'''
+    return render(request, "game/index.html", {
+        "tasks": tasks
+    })
+
+def add(request):
+    '''Listに要素を追加する'''
+    return render(request, "game/add.html")
+    
